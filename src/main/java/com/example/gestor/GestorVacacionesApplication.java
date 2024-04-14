e66b25d77bd8f4b3f8c8209a9986ef627586c094
nesscafe <40587996+nesscoffee@users.noreply.github.com>
2024-04-14 17:14:49 -0600

    Creacion de Spring Boot

---
+++ src/main/java/com/example/gestor/GestorVacacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorVacacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorVacacionesApplication.class, args);
+	}
+
+}
