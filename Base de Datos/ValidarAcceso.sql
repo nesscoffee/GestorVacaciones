58d5bc8dd621717b63369ddf530c2b56720c0d8c
Stephanie Sandoval <stephsandovalc@gmail.com>
2024-04-15 17:10:45 -0600

    intento de deshabilitar login

--- Base de Datos/ValidarAcceso.sql
+++ Base de Datos/ValidarAcceso.sql
@@ -27,32 +27,53 @@ BEGIN
 	SET NOCOUNT ON;
 	BEGIN TRY
 		-- declaracion de variables:
-		DECLARE @outResultCodeEvento INT;             -- para insertar eventos en la bitacora
-		DECLARE @inIDUsuario INT;                     -- para insertar eventos en la bitacora
+		DECLARE @outResultCodeEvento INT;                        -- para insertar eventos en la bitacora
+		DECLARE @inIDUsuario INT;                                -- para insertar eventos en la bitacora
+		DECLARE @intentosLogin INT;                              -- para verificar cuantas veces ha intentado ingresar
+		DECLARE @descripcion VARCHAR(128)                        -- para las descripciones de los eventos
 
 		-- inicializacion de variables:
 		SET @outResultCode = 0;
+
+		-- revisar cuantas veces ha intentado entrar el usuario
+		SELECT @intentosLogin = COUNT(*)
+		FROM (SELECT TOP 5 B.[IDTipoEvento], B.[PostTime]
+			  FROM BitacoraEvento B
+			  INNER JOIN TipoEvento T ON B.IDTipoEvento = T.ID
+			  WHERE T.Nombre = 'Login No Exitoso'                -- revisar que el ID corresponda a login no exitoso
+			  AND B.IDPostByUser IS NULL                         -- si login no es exitoso, el id de usuario deberia ser null
+			  AND B.PostTime >= DATEADD(MINUTE, -20, GETDATE())  -- revisar que las entradas hayan sucedido hace 20 min
+			  ORDER BY B.PostTime DESC                           -- ordenar por tiempo la tabla
+			 ) AS Subquery;
+
+		IF @intentosLogin >= 5
+		BEGIN
+			SET @outResultCode = 50003;                          -- error: login deshabilitado
+			EXEC dbo.IngresarEvento 'Login deshabilitado', NULL, ' ', @outResultCodeEvento OUTPUT;
+		END;
 	
 		-- revisar si el usuario existe en la base de datos, tabla Usuario:
-		IF NOT EXISTS (SELECT 1 FROM dbo.Usuario U WHERE U.Username = @inUsername)
-			BEGIN
-				SET @outResultCode = 50001;           -- error: usuario no existe
-				EXEC dbo.IngresarEvento 'Login No Exitoso', NULL, 'intentos: x, codigo: 50001', @outResultCodeEvento OUTPUT
-			END
+		IF @outResultCode = 0 AND NOT EXISTS (SELECT 1 FROM dbo.Usuario U WHERE U.Username = @inUsername)
+		BEGIN
+			SET @outResultCode = 50001;                          -- error: usuario no existe
+			SET @descripcion = (SELECT CONCAT ('intentos: ', CAST(@intentosLogin AS VARCHAR(1)), ', codigo 50001'));
+			EXEC dbo.IngresarEvento 'Login No Exitoso', NULL, @descripcion, @outResultCodeEvento OUTPUT;
+		END;
 
 		-- revisar que, dado que el usuario existe, la contrasena tambien existe:
 		IF @outResultCode = 0 AND NOT EXISTS (SELECT 1 FROM dbo.Usuario U WHERE U.Password = @inPassword)
-			BEGIN
-				SET @outResultCode = 50002;           -- error: contrasena no existe
-				EXEC dbo.IngresarEvento 'Login No Exitoso', NULL, 'intentos: x, codigo: 50002', @outResultCodeEvento OUTPUT
-			END
+		BEGIN
+			SET @outResultCode = 50002;                          -- error: contrasena no existe
+			SET @descripcion = (SELECT CONCAT ('intentos: ', CAST(@intentosLogin AS VARCHAR(1)), ', codigo 50002'));
+			EXEC dbo.IngresarEvento 'Login No Exitoso', NULL, @descripcion, @outResultCodeEvento OUTPUT;
+		END;
 
 		-- revisar que, dado que ambos datos existen, estos coincidan:
 		IF @outResultCode = 0 AND EXISTS (SELECT 1 FROM dbo.Usuario U WHERE U.Username = @inUsername AND U.Password = @inPassword)
-			BEGIN
-				SELECT @inIDUsuario = ID FROM Usuario U WHERE U.Username = @inUsername
-				EXEC dbo.IngresarEvento 'Login Exitoso', @inIDUsuario, ' ', @outResultCodeEvento OUTPUT
-			END
+		BEGIN
+			SELECT @inIDUsuario = ID FROM Usuario U WHERE U.Username = @inUsername;
+			EXEC dbo.IngresarEvento 'Login Exitoso', @inIDUsuario, ' ', @outResultCodeEvento OUTPUT;
+		END;
 
 		SELECT @outResultCode AS outResultCode;
 	END TRY
@@ -69,7 +90,7 @@ BEGIN
 			GETDATE()
 		);
 
-		SET @outResultCode = 50008;           -- error: problema base de datos
+		SET @outResultCode = 50008;                              -- error: problema base de datos
 
 	END CATCH;
 	SET NOCOUNT OFF;
